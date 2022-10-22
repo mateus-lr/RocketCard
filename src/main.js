4d0b9958786c55aeacd9e139422d44fa2535a0a8
Mateus Rocha <mateus_lr@outlook.com>
2022-10-22 02:20:53 -0300

    criação de masks, incluindo regex

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ function setCardType(type) {
 
   ccBgColor01.setAttribute("fill", colors[type][0])
   ccBgColor02.setAttribute("fill", colors[type][1])
-  ccLogo.setAttribute("src", `cc-${type}.svg`)  
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
 setCardType("default");
@@ -30,22 +30,54 @@ const securityCodeMasked = IMask(securityCode, securityCodePattern);
 const expirationDate = document.querySelector("#expiration-date");
 const expirationDatePattern = {
   mask: "MM{/}YY",
-  
-  blocks: {
-  MM: {
-    mask: IMask.MaskedRange,
-    from: 1,
-    to: 12,
-  },
 
-  YY: {
-    mask: IMask.MaskedRange,
-    from: String(new Date().getFullYear()).slice(2),
-    to: String(new Date().getFullYear() + 10).slice(2),
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    }
   }
-}
 }
 
 const expirationDateMasked = IMask(expirationDate, expirationDatePattern);
 
+const cardNumber = document.querySelector("#card-number");
+const cardNumberPattern = {
+  mask: [{
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
 
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    }
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex)
+    })
+
+    console.log(foundMask)
+
+    return foundMask
+  },
+}
+
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
