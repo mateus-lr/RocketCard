92a1a3bc3bfb69e612b11a1aec63af5d3fc0c3db
Mateus Rocha <mateus_lr@outlook.com>
2022-10-22 04:17:58 -0300

    adicionado função de atualização simultânea do cartão

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
 
 function setCardType(type) {
   const colors = {
-    "visa": ["#436D99", "#2D57F2"],
-    "mastercard": ["#DF6F29", "#C69347"],
+    "visa": ["#F7B600", "#1A1F71"],
+    "mastercard": ["#F79E1B", "#EB001B"],
     "default": ["black", "gray"],
   }
 
@@ -74,10 +74,55 @@ const cardNumberPattern = {
       return number.match(item.regex)
     })
 
-    console.log(foundMask)
-
     return foundMask
   },
 }
 
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
+
+const addButton = document.querySelector("#add-card");
+
+addButton.addEventListener("click", () => {
+  alert("Cartão adicionado com sucesso!");
+});
+
+document.querySelector("form").addEventListener("submit", event => {
+  event.preventDefault()
+})
+
+const cardHolder = document.querySelector("#card-holder")
+cardHolder.addEventListener("input", () => {
+  const ccHolder = document.querySelector(".cc-holder .value");
+
+  ccHolder.innerText = cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
+})
+
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value);
+})
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector(".cc-security .value")
+  ccSecurity.innerText = code.length === 0 ? "123" : code
+}
+
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setCardType(cardType);
+
+  updateCardNumber(cardNumberMasked.value);
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector(".cc-number")
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+}
+
+expirationDateMasked.on("accept", () => {
+  updateExpirationDate(expirationDateMasked.value);
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-expiration .value")
+  ccExpiration.innerText = date.length === 0 ? "02/32" : date
+}
